7bc90f83abaa0f2c7fb3eebe4b7ac6be8f5ae43d
qcp <prestidigitateur.VS@gmail.com>
2023-11-28 16:18:39 +0300

    chore: bump deps

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -12,8 +12,12 @@ import { openUrl } from '@/utils'
     <div style="flex-grow: 1" />
 
     <div class="social">
-      <ri-telegram-fill style="color: #3390ec" @click="openUrl('https://t.me/+TK1mucOyvZgzYzA6')" />
-      <ri-github-fill @click="openUrl('https://github.com/qcp')" />
+      <ri-telegram-fill
+        class="button"
+        style="color: #3390ec"
+        @click="openUrl('https://t.me/+TK1mucOyvZgzYzA6')"
+      />
+      <ri-github-fill @click="openUrl('https://github.com/qcp')" class="button" />
     </div>
 
     <div style="flex-grow: 1" />
@@ -27,5 +31,8 @@ import { openUrl } from '@/utils'
   gap: 0.5em;
 
   font-size: 2.5em;
+}
+.button {
+  cursor: pointer;
 }
 </style>
